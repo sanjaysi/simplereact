1fdd139cf0193c3509494b07b7f40b711de9d975
sanjaysi <cloudwithpython.com>
2016-09-17 20:59:25 +0530

    Refactored

--- simplereact/src/App.js
+++ simplereact/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
 
@@ -8,17 +7,19 @@ class App extends Component {
     this.state = {
       player_one_symbol: 'X',
       player_two_symbol: 'O',
+      gamestatus: 'new game',
       winningCombos: [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]],
       currentTurn: 'X',
       board: [
-        "","","","","","","","",""
+        null,null,null,null,null,null,null,null,null
       ]
     };
     this.handleClick = this.handleClick.bind(this);
+    this.onClickStart = this.onClickStart.bind(this);
   }
 
   handleClick(index) {
-    if (this.state.board[index] === "") {
+    if (this.state.board[index] === null) {
       let new_board = this.state.board;
       new_board[index] = this.state.currentTurn;
       let new_currentTurn = this.state.currentTurn === this.state.player_one_symbol ? this.state.player_two_symbol : this.state.player_one_symbol;
@@ -28,7 +29,7 @@ class App extends Component {
         currentTurn: new_currentTurn
       })
       if (this.checkForWinner()) {
-        alert('WON!!!');
+        this.setState({gamestatus: 'Game over'});
       }
     }
   }
@@ -36,24 +37,38 @@ class App extends Component {
   checkForWinner() {
     let board = this.state.board;
 
-    return this.state.winningCombos.find(function(combo) {
-      if (board[combo[0]] === board[combo[1]] &&
-            board[combo[1]] === board[combo[2]]) {
-              return board[combo[0]];
-          } else {
-            return false;
-          }
-    })
+    return (this.state.winningCombos.find(combo => this.checkmate(combo, board)));
+  }
+
+  checkmate(combo, board) {
+    let [x, y, z] = combo;
+
+    if ((board[x] === null) || (board[y] === null) || (board[z] === null)) {
+      return false;
+    } else if ((board[x] === board[y]) && (board[y] === board[z])) {
+      return true;
+    }
+  }
+
+  onClickStart() {
+    let new_board = this.state.board;
+    new_board.fill(null);
+    this.setState({board: new_board});
+    this.setState({gamestatus: 'New game'});
   }
 
   render() {
     return (
-      <div className="board">
-        {
-          this.state.board.map((cell, index) => {
-            return <div key={index} onClick={() => this.handleClick(index)} className="square">{cell}</div>
-          })
-        }
+      <div>
+        <button type="button" className="btn btn-default" onClick={this.onClickStart}>Start Game</button> 
+        <div>{this.state.gamestatus}</div>
+        <div className="board">
+          {
+            this.state.board.map((cell, index) => {
+              return <div key={index} onClick={() => this.handleClick(index)} className="square">{cell}</div>
+            })
+          }
+        </div>
       </div>
     );
   }
